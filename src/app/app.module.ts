9c0ad42f2003d7a0efd3fd124b092419bd70c3bb
Philip <creativepyt@gmail.com>
2020-04-19 02:19:01 +0200

    Ground work on the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainNavComponent } from './components/navbars/main-nav/main-nav.component';
+import { AttributionsComponent } from './components/attributions/attributions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainNavComponent,
+    AttributionsComponent
   ],
   imports: [
     BrowserModule,
