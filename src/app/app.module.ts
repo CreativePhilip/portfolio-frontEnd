b289a2a6eb86e3a684f81be255f4999c3f9305b2
Philip <creativepyt@gmail.com>
2020-04-25 16:14:55 +0200

    Footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MainNavComponent } from './components/navbars/main-nav/main-nav.component';
 import { AttributionsComponent } from './components/attributions/attributions.component';
+import { MainFooterComponent } from './components/footers/main-footer/main-footer.component';
+import { HttpClientModule } from "@angular/common/http";
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryCardComponent } from './components/category-card/category-card.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainNavComponent,
-    AttributionsComponent
+    AttributionsComponent,
+    MainFooterComponent,
+    CategoriesComponent,
+    CategoryCardComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
