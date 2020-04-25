123896a492e1aaf088c3b31a571eed70c05e6660
Philip <creativepyt@gmail.com>
2020-04-25 16:18:15 +0200

    Categories component

---
+++ src/app/pages/categories/categories.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {DatabaseService} from "../../services/database-connection/database.service";
+import {CategoryModel} from "../../services/database-connection/Models/CategoryModel";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent implements OnInit {
+  categories: CategoryModel[];
+
+  constructor(private db: DatabaseService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.db.getCategories().subscribe(value =>{
+      this.categories = value;
+    });
+  }
+
+  onCardClick(cat) {
+    this.router.navigateByUrl(`/categories/${cat.pk}`);
+  }
+
+}
